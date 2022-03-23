ddf8b41ffb8c5d0db66ec7629aa0bde09539fb84
huanmingchang <huanming1988@gmail.com>
2022-03-24 07:17:57 +0800

    feat: refactor and add UserCard component

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,10 +1,14 @@
 <script>
 import { ref, reactive, onMounted } from 'vue'
+import UserCard from '../components/UserCard.vue'
 import usersAPI from './../apis/users'
 import Swal from 'sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css'
 
 export default {
+  components: {
+    UserCard,
+  },
   setup() {
     const num = ref(10)
     const users = ref([])
@@ -27,7 +31,6 @@ export default {
         })
       }
     })
-    console.log(users)
 
     return { users }
   },
@@ -35,38 +38,8 @@ export default {
 </script>
 
 <template lang="pug">
-main.cards
-  .card(v-for="user in users")
-    .user-avatar
-      img.user-avatar-img(:src="user.picture.large")
-    .user-name {{user.name.first + ' ' +user.name.last}}
-    .user-location {{user.location.city + ', ' + user.location.country}}
+main
+  UserCard(:users="users")
 </template>
 
-<style lang="postcss" scoped>
-.cards {
-  @apply max-w-[1280px] h-screen grid grid-cols-auto-fit auto-rows-min gap-6 mx-10 my-10;
-  @apply md:mx-12;
-  @apply xl:mx-auto;
-}
-
-.card {
-  @apply flex flex-col items-center rounded-xl shadow-md bg-[#f4f2f3];
-}
-
-.user-avatar {
-  @apply w-11/12 mt-2 rounded-xl;
-}
-
-.user-avatar-img {
-  @apply w-full p-2 rounded-xl;
-}
-
-.user-name {
-  @apply text-xl font-bold mt-2;
-}
-
-.user-location {
-  @apply text-sm my-2;
-}
-</style>
+<style lang="postcss" scoped></style>
