288b2571d901dda39356aece83eaa993b3e92528
huanmingchang <huanming1988@gmail.com>
2022-04-17 09:46:29 +0800

    feat: add authentication function

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
-<script setup>
-</script>
+<script>
+import { useRouter, useRoute } from 'vue-router'
+import { onBeforeMount } from 'vue'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
+export default {
+  setup() {
+    const router = useRouter()
+    const route = useRoute()
+
+    onBeforeMount(() => {
+      const auth = getAuth()
+      onAuthStateChanged(auth, (user) => {
+        if (!user) {
+          router.push('/login')
+        } else if (route.path === '/login' || route.path === '/register') {
+          router.push('/')
+        }
+      })
+    })
+  },
+}
+</script>
 <template lang="pug">
 router-view
 </template>
